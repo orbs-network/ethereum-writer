4d0ff24203417789b8e7770f67a96b53091de282
Deddy Dayag <daviddayag@gmail.com>
2023-01-26 17:56:34 +0200

    commented out isGuardianInSync

--- src/read/guardians-reputations.ts
+++ src/read/guardians-reputations.ts
@@ -1,12 +1,6 @@
 import * as Logger from '../logger';
 import {Reputations, State} from '../model/state';
 import {getCurrentClockTime} from '../helpers';
-import {fetchManagementStatusWithTimeout, ManagementStatusResponse} from "./management";
-
-const GUARDIAN_FALLBACK_PORT = "18888";
-
-const guardiansSyncResults: { [address: string]: boolean[] } = {};
-
 /**
  * check sync of all guardians and updates state reputations
  *
@@ -34,7 +28,6 @@ export async function readAllGuardiansReputations(config: ReputationConfigParams
 
     // last to be after all possible exceptions and processing delays
     state.ReputationsLastPollTime = getCurrentClockTime();
-    Logger.log(`** DONE getCurrentClockTime`)
 
 
     // log progress
@@ -66,19 +59,23 @@ async function fetchGuardiansReputations(config: ReputationConfigParams, state: 
 
                 const orbsAddress = state.ManagementEthToOrbsAddress[ethAddress];
 
-                const _guardianSyncResults = guardiansSyncResults[ethAddress] = guardiansSyncResults[ethAddress] || []
+                /*const _guardianSyncResults = guardiansSyncResults[ethAddress] = guardiansSyncResults[ethAddress] || []
 
                 _guardianSyncResults.push(await isGuardianInSync(
                     ethAddress,
                     state,
                     config
                 ))
+                Logger.log(`** DONE isGuardianInSync`)
 
                 if (_guardianSyncResults.length > config.ReputationSampleSize) {
                     _guardianSyncResults.shift() // keep latest sample size
-                }
+                }*/
 
-                guardiansReputation[orbsAddress] = _guardianSyncResults.filter(rep => !rep).length;
+                // guardiansReputation[orbsAddress] = _guardianSyncResults.filter(rep => !rep).length;
+                guardiansReputation[orbsAddress] = 0;
+                config.ManagementServiceEndpointSchema;
+                await new Promise((resolve) => setTimeout(resolve, 1000));
 
             })()
         )
@@ -88,69 +85,3 @@ async function fetchGuardiansReputations(config: ReputationConfigParams, state: 
 
 }
 
-/**
- * check if guardian ethereum chain sync is valid
- *
- * @param ethAddress
- * @param state
- * @param config
- */
-async function isGuardianInSync(ethAddress: string, state: State, config: ReputationConfigParams): Promise<boolean> {
-
-    const now = getCurrentClockTime();
-
-    const response = await fetchMnmgnmtSrvStatusForGuardian(
-        ethAddress,
-        config,
-        state
-    );
-
-    return response !== null && response.Payload && (now - response.Payload.CurrentRefTime) < config.InvalidEthereumSyncSeconds;
-
-}
-
-
-/**
- * attempt to fetch & parse management service status from guardian
- *
- * @param ethAddress
- * @param config
- * @param state
- */
-async function fetchMnmgnmtSrvStatusForGuardian(ethAddress: string, config: ReputationConfigParams, state: State): Promise<ManagementStatusResponse | null> {
-
-    let response = null;
-
-    const guardian = state.ManagementCurrentTopology.find(guardian => guardian.EthAddress === ethAddress);
-
-    let managementServiceEndpoint = config.ManagementServiceEndpointSchema.replace(/{{GUARDIAN_IP}}/g, guardian?.Ip + "");
-
-    try {
-
-        response = await fetchManagementStatusWithTimeout(
-            managementServiceEndpoint
-        );
-
-    } catch (ignore) {
-
-        try {
-
-            Logger.log(`Cannot fetch management service status from guardian [${ethAddress}] on port [80], attempting to fetch over fallback port [${GUARDIAN_FALLBACK_PORT}]`);
-
-            managementServiceEndpoint = config.ManagementServiceEndpointSchema.replace(/{{GUARDIAN_IP}}/g, guardian?.Ip + ":" + GUARDIAN_FALLBACK_PORT);
-
-            response = await fetchManagementStatusWithTimeout(
-                managementServiceEndpoint
-            );
-
-        } catch (ignore) {
-
-            Logger.log(`Failed to fetch management service status from guardian [${ethAddress}], reporting bad reputation!`);
-
-        }
-
-    }
-
-    return response;
-
-}
