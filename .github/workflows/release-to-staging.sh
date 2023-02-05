bbbe21ba4a78190ce69dbb60869376a8cadfa47e
Deddy Dayag <daviddayag@gmail.com>
2023-02-05 12:37:11 +0200

    added github action for deployment

---
+++ .github/workflows/release-to-staging.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker login -u $DOCKER_HUB_STAGING_LOGIN -p $DOCKER_HUB_STAGING_PASSWORD
+
+export VERSION=$(cat .version)
+
+docker push orbsnetworkstaging/ethereum-writer:$VERSION
+
+if [[ $CIRCLE_BRANCH == "master" ]] ;
+then
+  docker tag orbsnetworkstaging/ethereum-writer:$VERSION orbsnetworkstaging/ethereum-writer:experimental
+  docker push orbsnetworkstaging/ethereum-writer:experimental
+fi
